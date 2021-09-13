a91ed8c3c2cbbe9f3ec5a1f75af1dc4cb181a396
AlbertVega <85046754+AlbertVega@users.noreply.github.com>
2021-09-12 21:41:10 -0600

    Create Cliente.java

---
+++ Sockets/src/sockets/Cliente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sockets;
+import java.net.*;
+import java.io.*;
+/**
+ *
+ * @author Albert Vega Camacho
+ */
+public class Cliente {
+    /**
+     * Attributes of client 
+     */
+    Socket client;
+    int port = 9000;
+    String ip = "192.168.0.16";
+    
+}
