76d55654a8450229fb451ab15975fcc61628f206
Zorrox0117 <83249731+Zorrox0117@users.noreply.github.com>
2021-09-21 22:19:29 -0700

    Revert "La que si sirve :v"
    This reverts commit 1d1f070351c0f5e0bf89bc5559ed325dc6d3e759.

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -8,8 +8,6 @@ public class Main {
     public static void main(String[] args) {
 
         VentanaPrincipal Menu = new VentanaPrincipal();
-
-        VentanaPrincipal Menu2 = new VentanaPrincipal();
             
     }
 
