32a4d89b0e8e23e95207c57508f0e02848f1527a
AlbertVega <85046754+AlbertVega@users.noreply.github.com>
2021-10-05 13:16:55 -0600

    Vamos tilin

--- src/main/java/Sockets/Logica.java
+++ src/main/java/Sockets/Logica.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 
 public class Logica {
     public static String respuesta;
-
+    public static Boolean esperando = true;
     public static DoubleNode recorrerCasillas(int pos, DoubleNode head) {
         DoubleNode node = head;
         for(int i = 0; i < pos; i++) {
@@ -18,24 +18,28 @@ public class Logica {
     }
 
     public static void MandarReto(DoubleNode Node) {
-        System.out.println("Tamo activo papi");
-        int a = Node.getA();
-        int b = Node.getB();
+        if (esperando) {
+            esperando = false;
+            System.out.println("Tamo activo papi");
+            int a = Node.getA();
+            int b = Node.getB();
 
-        switch (Node.getOperation()) {
-            case "+" -> respuesta = Integer.toString(a + b);
-            case "-" -> respuesta = Integer.toString(a - b);
-            case "*" -> respuesta = Integer.toString(a * b);
-            case "/" -> respuesta = Integer.toString(a / b);
-            default -> throw new IllegalStateException("Unexpected value: " + Node.getOperation());
+            switch (Node.getOperation()) {
+                case "+" -> respuesta = Integer.toString(a + b);
+                case "-" -> respuesta = Integer.toString(a - b);
+                case "*" -> respuesta = Integer.toString(a * b);
+                case "/" -> respuesta = Integer.toString(a / b);
+                default -> throw new IllegalStateException("Unexpected value: " + Node.getOperation());
+            }
+            VentanaReto.respuesta = respuesta;
+            VentanaReto.Operacion = a + Node.getOperation() + b;
+            VentanaReto reto = new VentanaReto();
         }
-        VentanaReto.respuesta = respuesta;
-        VentanaReto.Operacion = a + Node.getOperation() + b;
-        VentanaReto reto = new VentanaReto();
     }
 
     public static void turno(int num, DoubleNode head) {
         DoubleNode node = recorrerCasillas(num, head);
+        esperando = true;
         if(Objects.equals(node.getField(), "Reto")) {
             if (Objects.equals(VentanaPrincipal.conexion, "servidor")) {
                 Servidor.varPos1++;
